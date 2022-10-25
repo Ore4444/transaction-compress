1cc42f9da2117be1ab5f02120103cf0028aaabdb
Ore Landau <ore4444@gmail.com>
2022-10-25 17:08:14 +0300

    server logs

--- server/index.js
+++ server/index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/add', (req, res) => {
+  console.log('REQ: ' , req.body)  
+
+  const resp = 'Correct!'
+  res.send(resp)
+
+  console.log('RESP:' , resp)
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
